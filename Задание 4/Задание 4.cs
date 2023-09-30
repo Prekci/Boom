97c53c1b240083e09ad69edcffcb6047561c9ad9
Prekci <120401620+Prekci@users.noreply.github.com>
2023-09-30 11:16:16 +0300

    Add files via upload

---
+++ Задание 4/Задание 4.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем переменную для генерации случайных чисел
+            Random RandomMassive = new Random();
+            // Создаем массив для хранения 20 случайных чисел
+            int[] massive = new int[20];
+            // Заполняем массив случайными числами от 1 до 100
+            for (int i = 0; i < massive.Length; i++)
+            {
+                // Генерируем случайное число от 1 до 100
+                massive[i] = RandomMassive.Next(1, 101); 
+            }
+            // Объявляем переменные для хранения максимального и минимального значений
+            // Предполагая, что первый элемент массива - их начальные значения
+            int max = massive[0];
+            int min = massive[0];   
+            // Перебираем все элементы массива, чтобы найти максимальное и минимальное значения
+            foreach (int num in massive)
+            {
+                // Если текущее число больше текущего максимального значения, обновляем максимум
+                max = Math.Max(max, num);
+                // Если текущее число меньше текущего минимального значения, обновляем минимум
+                min = Math.Min(min, num);
+            }            
+            // Выводим все числа, разделенные пробелом
+            Console.WriteLine("Массив случайных чисел:\n" + string.Join(" ", massive)); 
+            // Выводим максимальное значение
+            Console.WriteLine($"Максимальное значение: {max}");
+            // Выводим минимальное значение
+            Console.WriteLine($"Минимальное значение: {min}");
+            // Выводим результаты на экран
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Задание 5/Задание 5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Задание_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Предлагаем ввести число
+            Console.WriteLine("Введите возраст:");
+            // Присваиваем переменной simbol значение и выводим на экран
+            string simbol = Console.ReadLine();
+            // Преобразовываем строку в число типа double
+            if (double.TryParse(simbol, out double age))
+            {
+                // Проверяем число на соответствие условию
+                if (age >= 18)
+                {
+                    // Если соответствует выводим на данное сообщение
+                    Console.WriteLine("Вы можете получить права");
+                    // Выводим на экран
+                    Console.ReadLine();
+                }
+                // Если не соответствует
+                else
+                {
+                    // Выводим это сообщение
+                    Console.WriteLine("Вы не можете получить права");
+                    // Выводим на экран
+                    Console.ReadLine();
+                }
+            }
+            // Если было введено что то кроме числа
+            else
+            {
+                // Выводим сообщение о некоректно введнном элементе
+                Console.WriteLine("Введите корректное значение");
+                // Выводим на экран
+                Console.ReadLine();
+            }
+        }
+    }
+}
